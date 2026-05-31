aaec82a53ced6ec43338901018f35cdd410ed9f8
nathaliafrederica-cell <nathaliafrederica@gmail.com>
2026-05-31 18:41:11 -0300

    Remove .env file creation instructions
    Removed instructions for creating a .env file.

--- src/script.js
+++ src/script.js
@@ -1,10 +1,6 @@
 const API_KEY = process.env.API_KEY || "";
 const DB_PASSWORD = process.env.DB_PASSWORD || "";
 
-// Then create a .env file (add to .gitignore):
-// API_KEY=your_actual_key_here
-// DB_PASSWORD=your_actual_password_here
-
 fetch('db.json')
     .then(response => response.json())
     .then(data => {
